e9997584d50b01bad55a4a6de2084c6b38f44088
server-frog <server@frogdigital.co>
2025-04-10 19:03:18 +0700

    edit file build.sh

--- nginx-php7.1/build.sh
+++ nginx-php7.1/build.sh
@@ -5,7 +5,11 @@
 # docker stop pat
 # docker rm pat
 # docker rmi pat
-docker build -t frogdigital/ubuntu:platform .
+docker build -t frogdigital/ubuntu:platform-$1 .
+docker tag frogdigital/ubuntu:platform-$1 frogdigital/ubuntu:platform-$1
+sleep 5
+
+ocker push frogdigital/ubuntu:platform-$1
 # docker run --name pat -v $PWD/apps:/var/www/html -p 80:80 -h lemp -d pat:latest
 # docker exec -ti pat bash
 
