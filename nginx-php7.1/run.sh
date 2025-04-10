78944c02a8c80cbf17255b39984e1015d1a967a5
server-frog <server@frogdigital.co>
2025-04-11 03:45:47 +0700

    edit file run.sh

--- nginx-php7.1/run.sh
+++ nginx-php7.1/run.sh
@@ -3,4 +3,17 @@
 chown -Rf www-data:www-data /var/www/html
 service nginx start
 service php7.1-fpm start
+
+touch /etc/php/7.1/fpm/snuffleupagus.rules
+echo 'sp.disable_function.function("eval").drop();' > /etc/php/7.1/fpm/snuffleupagus.rules
+
+touch /etc/php/7.1/fpm/conf.d/99-snuffleupagus.ini
+echo 'extension=snuffleupagus.so' > /etc/php/7.1/fpm/conf.d/99-snuffleupagus.ini
+echo 'sp.configuration_file=/etc/php/7.1/fpm/snuffleupagus.rules' >> /etc/php/7.1/fpm/conf.d/99-snuffleupagus.ini
+
+
+sed -i '/pattern="PDF"/s/rights="none"/rights="read | write"/' /etc/ImageMagick-6/policy.xml
+
+service php7.1-fpm restart
+
 while true; do sleep 60; done
