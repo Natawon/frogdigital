cefd9f50e16de41bb4c4fc0f75fc3c595ffe363e
server-frog <server@frogdigital.co>
2025-04-12 01:16:14 +0700

    fix file dockerfile & run.sh

--- nginx-php7.1/build.sh
+++ nginx-php7.1/build.sh
@@ -6,11 +6,11 @@
 # docker rm pat
 # docker rmi pat
 # docker build -t frogdigital/ubuntu:platform-$1 .
-docker build --platform=linux/amd64 -t frogdigital/ubuntu:platform-$1 .
-docker tag frogdigital/ubuntu:platform-$1 frogdigital/ubuntu:platform-$1
+docker build --platform=linux/amd64 -t frogdigital/ubuntu:nginx-php7.1-$1 .
+docker tag frogdigital/ubuntu:nginx-php7.1-$1 frogdigital/ubuntu:nginx-php7.1-$1
 sleep 5
 
-docker push frogdigital/ubuntu:platform-$1
+docker push frogdigital/ubuntu:nginx-php7.1-$1
 # docker run --name pat -v $PWD/apps:/var/www/html -p 80:80 -h lemp -d pat:latest
 # docker exec -ti pat bash
 
