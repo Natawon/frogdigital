6376d92d792ac0a549dc1c7568344753972bc805
server-frog <server@frogdigital.co>
2025-04-12 13:41:18 +0700

    fix update build

---
+++ nginx-php7.1/comment-dom.sh
@@ -0,0 +1,119 @@
+docker exec homepro sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec homepro sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec homepro sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec bitkub sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec bitkub sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec bitkub sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec yuanta sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec yuanta sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec yuanta sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec yamaha sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec yamaha sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec yamaha sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec unido sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec unido sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec unido sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec sec sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec sec sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec sec sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec premiumplm sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec premiumplm sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec premiumplm sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec physiology-sc sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec physiology-sc sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec physiology-sc sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec pccms sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec pccms sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec pccms sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec ntc-v4 sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec ntc-v4 sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec ntc-v4 sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec nineplanner sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec nineplanner sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec nineplanner sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec lukkid sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec lukkid sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec lukkid sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec jorakay sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec jorakay sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec jorakay sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec giz sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec giz sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec giz sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec gastalkth-v4 sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec gastalkth-v4 sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec gastalkth-v4 sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec bridgestone-captc sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec bridgestone-captc sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec bridgestone-captc sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec bearhouse sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec bearhouse sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec bearhouse sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec applicable sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec applicable sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec applicable sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec amland sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec amland sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec amland sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec supalai sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec supalai sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec supalai sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec bedrock sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec bedrock sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec bedrock sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec ohkajhu sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec ohkajhu sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec ohkajhu sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec shicualumni sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec shicualumni sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec shicualumni sh -c "service php7.1-fpm restart"
+echo "success"
+
+docker exec devdronemapper sh -c "sed -i 's/^;extension=dom\.so$/extension=dom.so/' /etc/php/7.1/cli/conf.d/20-dom.ini"
+docker exec devdronemapper sh -c "sed -i 's/^extension=dom\.so$/;extension=dom.so/' /etc/php/7.1/mods-available/dom.ini"
+docker exec devdronemapper sh -c "service php7.1-fpm restart"
+echo "success"
